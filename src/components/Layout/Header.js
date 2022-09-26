e1063b24b060ef1be574804810bd70ed231b27cd
marcosfil <marcosfilho222@gmail.com>
2022-09-26 16:07:39 +0200

    inicial commit

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,7 +1,20 @@
 import { Fragment } from "react";
 
+import mealsImage from "../../assets/meals2.jpg";
+import style from "./Header.module.css";
+
 const Header = (props) => {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <header className={style.header}>
+        <h1>React Meals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={style["main-image"]}>
+        <img src={mealsImage} alt="A woman eating food" />
+      </div>
+    </Fragment>
+  );
 };
 
 export default Header;
