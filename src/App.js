d381e7e00e9f20cb2b3e06fe8c29f48ecd96a6ea
marcosfil <marcosfilho222@gmail.com>
2022-09-27 17:34:13 +0200

    meals

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Fragment } from "react";
 
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <div>
       <Fragment>
         <Header />
+        <main>
+          <Meals />
+        </main>
       </Fragment>
     </div>
   );
